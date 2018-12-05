7b1f0b81fd52e8b0b456779f5f0aa40a3e16daf7
Marvourneen Dolor <marvourneen@mkdolor.com>
2018-12-05 12:56:36 -0500

    Update fCC Intermediate Algorithm Scripting.js

--- fCC Intermediate Algorithm Scripting.js
+++ fCC Intermediate Algorithm Scripting.js
@@ -468,3 +468,36 @@ function orbitalPeriod(arr) {
   return arr.map( (element) => ({"name":element["name"],
   "orbitalPeriod":Math.round(2 * Math.PI * Math.pow((Math.pow(6367.4447+element['avgAlt'], 3) / 398600.4418), 0.5))}) );
 }
+
+/*
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and
+backward, ignoring punctuation, case, and spacing.
+
+Note You'll need to remove all non-alphanumeric characters (punctuation, spaces
+and symbols) and turn everything into the same case (lower or upper case) in
+order to check for palindromes.
+
+We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race
+CAR" among others.
+
+We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and
+"2_A3*3#A2"
+*/
+function palindrome(str) {
+  let newStr = str.toLowerCase()
+  if (newStr.match(/[a-z0-9]/g)) {
+      newStr = newStr.match(/[a-z0-9]/g).join('')
+  }
+
+  if (newStr.length === 0 || newStr.length === 1) { // Base case
+      return true
+  } else {
+      if (newStr[0] !== newStr[newStr.length-1]){
+          return false
+      } else {
+          return palindrome(newStr.slice(1, newStr.length-1))
+      }
+  }
+}

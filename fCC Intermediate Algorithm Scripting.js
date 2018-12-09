0860a82821e084b39c9dceeb54e192699b449163
Marvourneen Dolor <marvourneen@mkdolor.com>
2018-12-09 13:39:24 -0500

    Update fCC Intermediate Algorithm Scripting.js
    Add references for fCC Javascript Algorithms and Data Structures projects

--- fCC Intermediate Algorithm Scripting.js
+++ fCC Intermediate Algorithm Scripting.js
@@ -470,34 +470,26 @@ function orbitalPeriod(arr) {
 }
 
 /*
-Return true if the given string is a palindrome. Otherwise, return false.
+fCC Javascript Algorithms and Data Structures: helpful references by project
 
-A palindrome is a word or sentence that's spelled the same way both forward and
-backward, ignoring punctuation, case, and spacing.
+Palindrome Checker
+RegEx Match: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
+Regursion: https://developer.mozilla.org/en-US/docs/Glossary/Recursion
+String Methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 
-Note You'll need to remove all non-alphanumeric characters (punctuation, spaces
-and symbols) and turn everything into the same case (lower or upper case) in
-order to check for palindromes.
+Roman Numeral Converter
+Replace: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+While: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
 
-We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race
-CAR" among others.
+Caesar's Cipher
+charCodeAt(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+fromCharCode(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
 
-We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and
-"2_A3*3#A2"
+Telephone Number Validator
+Regex test: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+Regex overview: https://eloquentjavascript.net/09_regexp.html
+
+Cash Register
+Objects: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
 */
-function palindrome(str) {
-  let newStr = str.toLowerCase()
-  if (newStr.match(/[a-z0-9]/g)) {
-      newStr = newStr.match(/[a-z0-9]/g).join('')
-  }
 
-  if (newStr.length === 0 || newStr.length === 1) { // Base case
-      return true
-  } else {
-      if (newStr[0] !== newStr[newStr.length-1]){
-          return false
-      } else {
-          return palindrome(newStr.slice(1, newStr.length-1))
-      }
-  }
-}
